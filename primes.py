d569fd925bc9855fb4ad7642058af9d515b8756d
Wesley Lin <wesley.lin@kcl.ac.uk>
2022-10-02 21:38:17 +0100

    Big dub

--- primes.py
+++ primes.py
@@ -2,5 +2,25 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    
+    if(number_of_primes <= 0):
+        raise ValueError("Invalid value")
+    
     list = []
+    factors = 0
+    i = 2
+    while len(list) < number_of_primes:
+        
+        if prime_finder(i):
+            list.append(i)  
+        i = i + 1          
+            
     return list
+
+def prime_finder(i):
+   
+    for j in range(2, i):
+        if(i % j == 0):
+            return False
+    
+    return True
